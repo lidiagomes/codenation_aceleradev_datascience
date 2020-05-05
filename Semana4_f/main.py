0caecd50b722a2cbbe1f0b1a11d48cf51f157952
Lidia Gomes <lidia.moreira036@gmail.com>
2020-05-05 14:00:22 -0300

    renomeando

--- Semana4_f/main.py
+++ Semana4_f/main.py
@@ -28,7 +28,7 @@ from statsmodels.distributions.empirical_distribution import ECDF
 # In[2]:
 
 
-#%matplotlib inline
+get_ipython().run_line_magic('matplotlib', 'inline')
 
 from IPython.core.pylabtools import figsize
 
@@ -59,129 +59,17 @@ dataframe = pd.DataFrame({"normal": sct.norm.rvs(20, 4, size=10000),
 # Sua análise da parte 1 começa aqui.
 
 
-# In[5]:
-
-
-# plotando o gráfico da normal
-sns.distplot(sct.norm.rvs(20, 4, size=10000))
-
-
-# In[6]:
-
-
-sns.distplot(sct.binom.rvs(100, 0.2, size=10000))
-
-
-# ### 1. valores básicos das distribuições
-
-# In[7]:
-
-
-dic_normal =pd.DataFrame({
-'média':dataframe['normal'].mean(),
-'mediana':dataframe['normal'].median(),
-'desv_padrao':dataframe['normal'].std(), 
-'maior_valor':dataframe['normal'].max(),
-'menor_valor':dataframe['normal'].min(),
-'variancia':dataframe['normal'].var()},index = [0])
-dic_normal
-
-
-# In[8]:
-
-
-dic_binomial =pd.DataFrame({
-'média':dataframe['binomial'].mean(),
-'mediana':dataframe['binomial'].median(),
-'desv_padrao':dataframe['binomial'].std(), 
-'maior_valor':dataframe['binomial'].max(),
-'menor_valor':dataframe['binomial'].min(),
-'variancia':dataframe['binomial'].var()},index = [0])
-dic_binomial
-
-
-# In[9]:
-
-
-# calculando os percentis para a distribuição normal
-q1_norm = sct.scoreatpercentile(dataframe['normal'],25)
-q2_norm = sct.scoreatpercentile(dataframe['normal'],50)
-q3_norm = sct.scoreatpercentile(dataframe['normal'],75)
-q1_norm, q2_norm, q3_norm
-
-
-# In[10]:
-
-
-# calculando os percentis para a distribuição binomial
-q1_binom = sct.scoreatpercentile(dataframe['binomial'],25)
-q2_binom = sct.scoreatpercentile(dataframe['binomial'],50)
-q3_binom = sct.scoreatpercentile(dataframe['binomial'],75)
-q1_binom, q2_binom, q3_binom
-
-
-# In[11]:
-
-
-# média, variância e desvio padrão das distribuições
-m_norm = dataframe['normal'].mean()
-v_norm = dataframe['normal'].var()
-desv_p_norm = dataframe['normal'].std()
-m_binom = dataframe['binomial'].mean()
-v_binom = dataframe['binomial'].var()
-desv_p_binom = dataframe['binomial'].std()
-
-
-# ##### Questão 1
-
-# In[12]:
-
-
-quartil_normal = np.percentile(dataframe['normal'], [25, 50, 75])
-quartil_binomial = np.percentile(dataframe['binomial'], [25, 50, 75])
-dif_quartis = tuple(np.around(quartil_normal-quartil_binomial, 3))
-dif_quartis
-
-
-# In[13]:
-
-
-#Definindo os intervalos
-z1 = m_norm - desv_p_norm
-z2 = m_norm + desv_p_norm
-z1, z2
-
-
-# In[14]:
-
-
-#calculando a CDF empírica 
-ecdf = ECDF(dataframe['normal'])
-prob = ecdf(z2)-ecdf(z1)
-prob
-
-
-# In[16]:
-
-
-# diferênça entre as médias e variâncias
-round(m_binom - m_norm,3),round(v_binom - v_norm,3)
-
-
 # ## Questão 1
 # 
 # Qual a diferença entre os quartis (Q1, Q2 e Q3) das variáveis `normal` e `binomial` de `dataframe`? Responda como uma tupla de três elementos arredondados para três casas decimais.
 # 
 # Em outra palavras, sejam `q1_norm`, `q2_norm` e `q3_norm` os quantis da variável `normal` e `q1_binom`, `q2_binom` e `q3_binom` os quantis da variável `binom`, qual a diferença `(q1_norm - q1 binom, q2_norm - q2_binom, q3_norm - q3_binom)`?
 
-# In[17]:
+# In[5]:
 
 
 def q1():
-    quartil_normal = np.percentile(dataframe['normal'], [25, 50, 75])
-    quartil_binomial = np.percentile(dataframe['binomial'], [25, 50, 75])
-    dif_quartis = tuple(np.around(quartil_normal-quartil_binomial, 3))
-    return dif_quartis
+    # Retorne aqui o resultado da questão 1.
     pass
 
 
@@ -195,15 +83,11 @@ def q1():
 # 
 # Considere o intervalo $[\bar{x} - s, \bar{x} + s]$, onde $\bar{x}$ é a média amostral e $s$ é o desvio padrão. Qual a probabilidade nesse intervalo, calculada pela função de distribuição acumulada empírica (CDF empírica) da variável `normal`? Responda como uma único escalar arredondado para três casas decimais.
 
-# In[18]:
+# In[6]:
 
 
 def q2():
-    z1 = m_norm - desv_p_norm
-    z2 = m_norm + desv_p_norm
-    ecdf = ECDF(dataframe['normal'])
-    prob = ecdf(z2)-ecdf(z1)
-    return round(float(prob),3)
+    # Retorne aqui o resultado da questão 2.
     pass
 
 
@@ -218,25 +102,24 @@ def q2():
 # 
 # Em outras palavras, sejam `m_binom` e `v_binom` a média e a variância da variável `binomial`, e `m_norm` e `v_norm` a média e a variância da variável `normal`. Quais as diferenças `(m_binom - m_norm, v_binom - v_norm)`?
 
-# In[19]:
+# In[7]:
 
 
 def q3():
-    dif_mean_var = round(m_binom - m_norm,3),round(v_binom - v_norm,3)
-    return dif_mean_var
+    # Retorne aqui o resultado da questão 3.
     pass
 
 
 # Para refletir:
 # 
-# * Você esperava valores dessa magnitude?
+# * Você esperava valore dessa magnitude?
 # * Qual o efeito de aumentar ou diminuir $n$ (atualmente 100) na distribuição da variável `binomial`?
 
 # ## Parte 2
 
 # ### _Setup_ da parte 2
 
-# In[20]:
+# In[8]:
 
 
 stars = pd.read_csv("pulsar_stars.csv")
@@ -253,80 +136,10 @@ stars.loc[:, "target"] = stars.target.astype(bool)
 
 # ## Inicie sua análise da parte 2 a partir daqui
 
-# In[21]:
+# In[9]:
 
 
 # Sua análise da parte 2 começa aqui.
-
-
-# In[22]:
-
-
-stars.head()
-
-
-# In[23]:
-
-
-stars.describe()
-
-
-# In[24]:
-
-
-#filtrando valores de interesse
-aux=stars.loc[stars['target']==False]
-
-
-# In[25]:
-
-
-mean_profile = aux['mean_profile']
-
-
-# In[26]:
-
-
-mean_profile.head()
-
-
-# In[27]:
-
-
-mean_profile.describe()
-
-
-# In[28]:
-
-
-#padronizando a variável mean_profile
-false_pulsar_mean_profile_standardized = ((mean_profile - mean_profile.mean())/mean_profile.std())
-
-
-# In[29]:
-
-
-#calculando os quantis teóricos para os valores apresentados
-quantis_teoricos = tuple(sct.norm.ppf([0.80,0.90,0.95],0,1))
-quantis_teoricos
-
-
-# In[30]:
-
-
-# calculando a probabilidade associada aos quantis_teoricos
-prob = ECDF(false_pulsar_mean_profile_standardized)(quantis_teoricos).round(3)
-prob
-
-
-# In[31]:
-
-
-# calculo de diferenças entre quantis
-quantil_normal = sct.norm.ppf([0.25, 0.50, 0.75],0,1)
-quantil_teorico = np.percentile(false_pulsar_mean_profile_standardized, [25, 50, 75])
-dif_quantis = tuple(np.around(quantil_teorico - quantil_normal, 3))
-dif_quantis
 
 
 # ## Questão 4
@@ -342,16 +155,11 @@ dif_quantis
 # 
 # Quais as probabilidade associadas a esses quantis utilizando a CDF empírica da variável `false_pulsar_mean_profile_standardized`? Responda como uma tupla de três elementos arredondados para três casas decimais.
 
-# In[32]:
+# In[10]:
 
 
 def q4():
-    aux=stars.loc[stars['target']==False]
-    mean_profile = aux['mean_profile']
-    false_pulsar_mean_profile_standardized = ((mean_profile - mean_profile.mean())/mean_profile.std())
-    quantis_teoricos = tuple(sct.norm.ppf([0.80,0.90,0.95],0,1))
-    prob = ECDF(false_pulsar_mean_profile_standardized)(quantis_teoricos).round(3)
-    return tuple(prob)
+    # Retorne aqui o resultado da questão 4.
     pass
 
 
@@ -364,17 +172,11 @@ def q4():
 # 
 # Qual a diferença entre os quantis Q1, Q2 e Q3 de `false_pulsar_mean_profile_standardized` e os mesmos quantis teóricos de uma distribuição normal de média 0 e variância 1? Responda como uma tupla de três elementos arredondados para três casas decimais.
 
-# In[33]:
+# In[11]:
 
 
 def q5():
-    aux=stars.loc[stars['target']==False]
-    mean_profile = aux['mean_profile']
-    false_pulsar_mean_profile_standardized = ((mean_profile - mean_profile.mean())/mean_profile.std())
-    quantil_normal = sct.norm.ppf([0.25, 0.50, 0.75],0,1)
-    quantil_teorico = np.percentile(false_pulsar_mean_profile_standardized, [25, 50, 75])
-    dif_quantis = tuple(np.around(quantil_teorico - quantil_normal, 3))
-    return dif_quantis
+    # Retorne aqui o resultado da questão 5.
     pass
 
 
